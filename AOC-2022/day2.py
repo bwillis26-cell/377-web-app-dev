47151621bc43375adef0499ed42c5e3f25c7a940
Ben Willis <bwillis26@hanoverstudents.org>
2025-10-06 11:55:16 -0400

    AOC Day 2

--- AOC-2022/day2.py
+++ AOC-2022/day2.py
@@ -26,6 +26,39 @@ for line in lines:
             score += 9 
         else:
             score += 6
-print(score)
+print("Part 1: " + str(score))
+
+rock = 1
+paper = 2
+scissors = 3
+score = 0
+for line in lines:
+    line = line.strip().split(" ")
+    if line[1] == "Z":
+        score += 6
+        if line[0] == "A":
+            score += 2
+        if line[0] == "B":
+            score += 3
+        if line[0] == "C":
+            score += 1
+
+    elif line[1] == "Y":
+        score += 3
+        if line[0] == "A":
+            score += 1
+        if line[0] == "B":
+            score += 2
+        if line[0] == "C":
+            score += 3
+
+    else:
+        if line[0] == "A":
+            score += 3
+        if line[0] == "B":
+            score += 1
+        if line[0] == "C":
+            score += 2
+print("Part 2: " + str(score))
     
     
