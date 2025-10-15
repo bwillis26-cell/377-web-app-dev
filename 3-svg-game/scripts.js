b89a6e9c2c7f2383cf591d7a622d525d23dab6e2
Ben Willis <bwillis26@hanoverstudents.org>
2025-10-15 14:36:40 -0400

    Trying some JS code for checkers

--- 3-svg-game/scripts.js
+++ 3-svg-game/scripts.js
@@ -1,3 +1,23 @@
-function selectPiece() {
-    
+turn = true
+
+
+function selectPiece(pieceID) {
+    var color = pieceID.charAt(0)
+    var xValue = $('#' + pieceID).attr('x');
+    var yValue = $('#' + pieceID).attr('y');
+    if (turn) {
+        if (color == "b") {
+            var turn = false;
+        } else {
+            console.log("Wrong piece or Not your turn! Select a different piece");
+        }
+
+    } else {
+        if (color == "r") {
+            turn = true;
+        } else {
+            console.log("Wrong piece or Not your turn! Select a different piece");
+        }
+    }
 }
+
