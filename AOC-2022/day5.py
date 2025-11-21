e136ab4d8767380566baa84b0788127df28bd061
Ben Willis <bwillis26@hanoverstudents.org>
2025-11-21 13:54:42 -0500

    Day5 AOC p2

--- AOC-2022/day5.py
+++ AOC-2022/day5.py
@@ -17,18 +17,42 @@ mainStacks = [['F', 'T', 'C', 'L', 'R', 'P', 'G', 'Q'],
               ['Q', 'N', 'B', 'G', 'L', 'S', 'P', 'H'] ]
 
 
+# for line in lines:
+#     line = line.strip()
+#     splitList = line.split(" ")
+#     numBlocks = int(splitList[1])
+#     numFrom = int(splitList[3]) - 1
+#     numTo = int(splitList[5]) - 1
+
+#     for i in range(numBlocks):
+
+
+#         block = mainStacks[numFrom].pop()
+#         mainStacks[numTo].append(block)
+
+# for i in range(len(mainStacks)):
+#     print(mainStacks[i][-1], end='')
+
+
 for line in lines:
     line = line.strip()
     splitList = line.split(" ")
     numBlocks = int(splitList[1])
     numFrom = int(splitList[3]) - 1
     numTo = int(splitList[5]) - 1
-
+    tempList = []
     for i in range(numBlocks):
-
-
         block = mainStacks[numFrom].pop()
-        mainStacks[numTo].append(block)
+        tempList.append(block)
+       
+    tempList.reverse()
+    for j in range(len(tempList)):
+        mainStacks[numTo].append(tempList[j])
+
+
+
+
+
 
 for i in range(len(mainStacks)):
     print(mainStacks[i][-1], end='')
