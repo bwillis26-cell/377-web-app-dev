95d94ee79037202c7c58f5f77647ba4ac813746b
Ben Willis <bwillis26@hanoverstudents.org>
2025-12-02 08:21:30 -0500

    day1 solution, got day2 ready

--- AOC-2025/day1.py
+++ AOC-2025/day1.py
@@ -1,40 +1,31 @@
-file = open('day1-test.txt', 'r')
+file = open('day1.txt', 'r')
 lines = file.readlines()
 
 dial = 50
-count = 0
+count1 = 0
+count2 = 0
 for line in lines:
     line = line.strip()
     
-    if len(line) == 2:
-        amount = int(line[1])
-    elif len(line) == 3:
-        amount = int(line[1:3])
-    else:
-        amount = int(line[1:4])
+    amount = int(line[1:])
 
     direction = line[0]
 
-    if direction == "L":
-        dial -= amount
-        
-    else:
-        dial += amount
-        
-    
-    print(dial)
-    
-    dial = dial % 100
-    print(dial)
-    dial = abs(dial)
-    print("     ")
-
-    count += (amount // 100)
+    for i in range(amount):
+        if direction == "L":
+            dial = (dial - 1 + 100) % 100
+        else:
+            dial = (dial + 1) % 100
+        if dial == 0:
+            count2 += 1
+    if dial == 0:
+        count1 += 1
     
     
     
     
-print(count)
+    
+print(count2)
 
 
 
---
+++ AOC-2025/day2.py
@@ -0,0 +1,6 @@
+file = open('day1.txt', 'r')
+lines = file.readlines()
+
+
+for line in lines:
+    line = line.strip()
