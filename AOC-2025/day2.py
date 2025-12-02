c64457a29c046bc059126252f06429035c73f14f
Ben Willis <bwillis26@hanoverstudents.org>
2025-12-02 12:41:06 -0500

    Day 2 done

--- AOC-2025/day2.py
+++ AOC-2025/day2.py
@@ -1,6 +1,25 @@
-file = open('day1.txt', 'r')
-lines = file.readlines()
+file = open('day2.txt', 'r')
+line = file.readlines()[0].strip(",")
 
+invalidID = []
 
-for line in lines:
-    line = line.strip()
+sum = 0
+
+ids = line.split(",")
+
+for id in ids:
+    start, end = (int(x) for x in id.split("-"))
+    for i in range(start, end + 1):
+        quags = str(i)
+        length = len(quags)
+        for j in range(1, length):
+            if length % j == 0:
+                chunks = [quags[k:k + j] for k in range(0, len(quags), j)]
+                if len(set(chunks)) == 1:
+                    sum += i
+                    break
+        
+            
+
+    
+print(sum)
