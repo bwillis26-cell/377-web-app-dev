1e06ea9e2f1fded4080ddcd37834e6c9e0abc78a
Ben Willis <bwillis26@hanoverstudents.org>
2025-12-03 08:28:25 -0500

    day 3 p 1 done

--- AOC-2025/day3.py
+++ AOC-2025/day3.py
@@ -1,4 +1,4 @@
-file = open('day3-test.txt', 'r')
+file = open('day3.txt', 'r')
 lines = file.readlines()
 
 total = 0
@@ -16,7 +16,10 @@ for line in lines:
     tempList = []
     tempString = ""
     for i in range(2, len(line)):
-        if line[i] > max2:
+        if max2 > max1:
+            max1 = max2
+            max2 = line[i]        
+        elif line[i] > max2:
             if max2 > max1:
               max1 = max2
               max2 = line[i]
@@ -26,7 +29,7 @@ for line in lines:
     tempList.append(max1)
     tempList.append(max2)
     tempString = tempList[0] + tempList[1]
-    print(tempString)
+    
     total += int(tempString)
             
 
