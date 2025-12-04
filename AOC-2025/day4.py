0d3fea0dcbd1815e80b57dc2ab96acebb6e5ac69
Ben Willis <bwillis26@hanoverstudents.org>
2025-12-04 12:46:56 -0500

    day4 p2 not working

--- AOC-2025/day4.py
+++ AOC-2025/day4.py
@@ -1,12 +1,113 @@
 file = open('day4-test.txt', 'r')
 lines = file.readlines()
 
+def count_neighbors(y, x):
+    atCount = 0
+    if y == lengthGrid - 1:
+        if x != 0 and x != lengthRow - 1:
+            if grid[y][x - 1] != ".":
+                atCount += 1
+            if grid[y][x + 1] != ".":
+                atCount += 1
+            if grid[y - 1][x] != ".":
+                atCount += 1
+            if grid[y - 1][x - 1] != ".":
+                atCount += 1
+            if grid[y - 1][x + 1] != ".":
+                atCount += 1
+    elif y == 0:
+        if x != 0 and x != lengthRow - 1:
+            if grid[y][x - 1] != ".":
+                atCount += 1
+            if grid[y][x + 1] != ".":
+                atCount += 1
+            if grid[y + 1][x] != ".":
+                atCount += 1
+            if grid[y + 1][x - 1] != ".":
+                atCount += 1
+            if grid[y + 1][x + 1] != ".":
+                atCount += 1
+    else:
+        if x == 0:
+            if grid[y][x + 1] != ".":
+                atCount += 1
+            if grid[y - 1][x + 1] != ".":
+                atCount += 1
+            if grid[y + 1][x] != ".":
+                atCount += 1
+            if grid[y - 1][x] != ".":
+                atCount += 1
+            if grid[y + 1][x + 1] != ".":
+                atCount += 1
+        elif x == lengthRow - 1:
+            if grid[y][x - 1] != ".":
+                atCount += 1
+            if grid[y - 1][x - 1] != ".":
+                atCount += 1
+            if grid[y + 1][x] != ".":
+                atCount += 1
+            if grid[y - 1][x] != ".":
+                atCount += 1
+            if grid[y + 1][x - 1] != ".":
+                atCount += 1
+        else:
+            if grid[y - 1][x] != ".":
+                atCount += 1
+            if grid[y - 1][x + 1] != ".":
+                atCount += 1
+            if grid[y - 1][x - 1] != ".":
+                atCount += 1
+            if grid[y][x - 1] != ".":
+                atCount += 1
+            if grid[y][x + 1] != ".":
+                atCount += 1
+            if grid[y + 1][x - 1] != ".":
+                atCount += 1
+            if grid[y + 1][x] != ".":
+                atCount += 1
+            if grid[y + 1][x + 1] != ".":
+                atCount += 1
+
+    return atCount
+
+grid = []
 
 for line in lines:
-    line = line.strip()
+    row = [x for x in line.strip()]
+    grid.append(row)
 
-        
+totalAt = 0
+lengthGrid = len(grid)
+lengthRow = len(grid[0])
+while True:
+    for y in range(lengthGrid):
+        for x in range(lengthRow): 
+            if grid[y][x] != ".":    
+                if count_neighbors(y, x) < 4:
+                    totalAt += 1
+                    grid[y][x] = "X"
+    for j in range(lengthGrid):
+        for i in range(lengthRow):
+            if grid[j][i] == "X":
+                grid[j][i] == "."
+    
+    if totalAt == 0:
+        break
+    totalAt = 0
+
+totalP2 = 0
+for y in range(lengthGrid):
+    for x in range(lengthRow):
+        if grid[y][x] == "@":
+            totalP2 += 1
+
+print(totalP2)
+            
 
 
 
+
+
+# for row in grid:
+#     print("".join(row))
 
