1067635bc94b8c7783587ac8a03cfae5e9a52cc6
Ben Willis <bwillis26@hanoverstudents.org>
2026-02-03 10:53:03 -0500

    quick save to refresh

---
+++ hmdb/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title></title>
+    </head>
+
+    <body>
+        <h1>hMDB: The Hanover Movie Database</h1>
+
+        <h2>Movies</h2>
+
+        list the movies here!
+    </body>
+</html>
