86deab8bbeabde2b5b43aff2b0323526a1312b69
Ben Willis <bwillis26@hanoverstudents.org>
2026-02-06 12:35:40 -0500

    might work for p3

--- project-euler/p-3.py
+++ project-euler/p-3.py
@@ -1,8 +1,12 @@
 import math
 
-max = 0
+max = -1
 start = 600851475143
-startSqrt =(int) (math.sqrt(start) + 1)
+startSqrt = int(math.sqrt(start) + 1)
+
+while start % 2 == 0:
+    max = 2
+    start //= 2
 
 
 for i in range(3, startSqrt, 2):
