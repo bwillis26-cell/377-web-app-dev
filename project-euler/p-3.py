04ba7619f0c625d61800784f46ee6b86f87d404b
Ben Willis <bwillis26@hanoverstudents.org>
2026-02-06 12:36:44 -0500

    p3 doesnt work

--- project-euler/p-3.py
+++ project-euler/p-3.py
@@ -2,12 +2,12 @@ import math
 
 max = -1
 start = 600851475143
-startSqrt = int(math.sqrt(start) + 1)
 
 while start % 2 == 0:
     max = 2
     start //= 2
 
+startSqrt = int(math.sqrt(start) + 1)
 
 for i in range(3, startSqrt, 2):
     while start % i == 0:
