069d8cdd4496987fe1560478a0066fa5a8401a8f
Ben Willis <bwillis26@hanoverstudents.org>
2026-02-12 12:01:16 -0500

    updated hmdb from ciccolo

--- hmdb/detail.php
+++ hmdb/detail.php
@@ -1,29 +1,62 @@
 <?php
 
-$connection = get_connection();
+/*************************************************************************************************
+ * detail.php
+ *
+ * Displays the details for a single movie. This page expects to be included within index.php.
+ *************************************************************************************************/
 
 $sql =<<<SQL
 SELECT *
-FROM movie
-WHERE mov_id = $id
+  FROM movie
+ WHERE mov_id = $id
 SQL;
 
+$connection = get_connection();
 
+// Run the query on the database
 $result = $connection->query($sql);
+
+// Store the ONE result in an associative array
 $row = $result->fetch_assoc();
 
-echo "<a href='index.php'>Back to Full Database</a> ";
-
-echo "<table border=1>";
-
-echo "<tr><th>ID</th><td><input type='text' value='" . $row['mov_id'] . "'></input></td></tr>"; 
-echo "<tr><th>Title</th><td><input type='text' value='" . $row['mov_title'] . "'></input></td></tr>";
-echo "<tr><th>Duration</th><td><input type='text' value='" . $row['mov_duration'] . "'></input></td></tr>";
-echo "<tr><th>Release Year</th><td><input type='text' value='" . $row['mov_release_year'] . "'></input></td></tr>";
-echo "<tr><th>MPAA Rating</th><td><input type='text' value='" . $row['mov_mpaa'] . "'></input></td></tr>";
-echo "<tr><th>Star Rating</th><td><input type='text' value='" . $row['mov_rating'] . "'></input></td></tr>";
-echo "<tr><th>Genre</th><td><input type='text' value='" . $row['mov_genre'] . "'></input></td></tr>";
-
-echo "</table>";
-
 ?>
+
+<h2><?php echo $row["mov_title"]; ?></h2>
+
+<div class="mb-3">
+    <label for="id" class="form-label">ID</label>
+    <input type="text" class="form-control" name="id" value="<?php echo $row["mov_id"]; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="title" class="form-label">Title</label>
+    <input type="text" class="form-control" name="title" value="<?php echo $row["mov_title"]; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="genre" class="form-label">Genre</label>
+    <input type="text" class="form-control" name="genre" value="<?php echo $row["mov_genre"]; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="rating" class="form-label">Rating</label>
+    <input type="text" class="form-control" name="rating" value="<?php echo $row["mov_rating"]; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="mpaa" class="form-label">MPAA</label>
+    <input type="text" class="form-control" name="mpaa" value="<?php echo $row["mov_mpaa"]; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="duration" class="form-label">Duration</label>
+    <input type="text" class="form-control" name="duration" value="<?php echo $row["mov_duration"]; ?>">
+</div>
+
+<div class="mb-3">
+    <label for="release_year" class="form-label">Release Year</label>
+    <input type="text" class="form-control" name="release_year" value="<?php echo $row["mov_release_year"]; ?>">
+</div>
+
+<a href="index.php?content=list" class="btn btn-secondary" role="button">Cancel</a>
--- hmdb/list.php
+++ hmdb/list.php
@@ -1,64 +1,88 @@
+<?php
 
-<h2>Movies <span id='record-count'></span></h2>
+/*************************************************************************************************
+ * list.php
+ *
+ * Displays a list of movies. This page expects to be included within index.php.
+ *************************************************************************************************/
 
-<?php 
-for ($i = 1; $i < 10; $i++) {
-    echo "<a href='index.php?nav=list&filter=$i'>$i</a> ";
-}
-
-
-for ($i = 0; $i < 26; $i++) {
-    $letter = chr($i + ord("A"));
-    echo "<a href='index.php?nav=list&filter=$letter'>$letter</a> ";
-}
 ?>
 
-<a href='index.php?nav=list'>All</a>
+<h2>Movies <span id="record-count"></span></h2>
 
-<table border=1>
-    <tr>
-        <th>Title</th>
-        <th>Duration</th>
-        <th>Release Date</th>
-<?php 
+<a href='index.php?content=list'>All</a>
+
+<?php
+
+for ($i = 1; $i < 10; $i++)
+{
+    echo "<a href='index.php?content=list&filter=$i'>$i</a> ";
+}
+
+for ($i = 0; $i < 26; $i++)
+{
+    $letter = chr($i + ord("A"));
+    echo "<a href='index.php?content=list&filter=$letter'>$letter</a> ";
+}
+
+?>
+
+<table class="table table-bordered table-hover">
+    <thead class="thead-dark">
+        <tr>
+            <th>Title</th>
+            <th>Duration</th>
+            <th>Release</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
 
 $connection = get_connection();
 
-if (!isset($filter)) {
+if (!isset($filter))
+{
     $filter = '';
-} else {
+}
+else
+{
     $filter = $connection->real_escape_string($filter);
 }
 
 $sql =<<<SQL
-SELECT * 
-FROM movie
-WHERE mov_title LIKE '$filter%'
-ORDER BY mov_title
+ SELECT *
+   FROM movie
+  WHERE mov_title LIKE '$filter%'
+  ORDER BY mov_title
 SQL;
 
-$count = 0;
-
+$recordCount = 0;
 $result = $connection->query($sql);
-while ($row = $result->fetch_assoc()) {
-
+while ($row = $result->fetch_assoc())
+{
     echo "<tr>";
-    echo "<td><a href='index.php?nav=detail&id=$row[mov_id]'>" . $row["mov_title"] . "</a></td>";
+    echo "<td><a href='index.php?content=detail&id=". $row["mov_id"] . "'>" . $row["mov_title"] . "</a></td>";
     echo "<td>" . $row["mov_duration"] . "</td>";
     echo "<td>" . $row["mov_release_year"] . "</td>";
     echo "</tr>";
-    $count++;
+
+    $recordCount++;
 }
 
 ?>
+
+    </tbody>
 </table>
-<?php 
-        
+
+<?php
+
 $code =<<<JS
 <script>
-document.getElementById('record-count').innerHTML = '(' + $count + ' records)';
+document.getElementById('record-count').innerHTML = '(' + $recordCount + ' records)';
 </script>
 JS;
-        
+
 echo $code;
+
 ?>
